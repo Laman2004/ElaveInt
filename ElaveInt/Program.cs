42991e2df57e19635a956bd8220221fe27e5d546
Leman <Leman@COMPUTER-SCIENC>
2023-03-22 12:50:40 +0400

    Add project files.

---
+++ ElaveInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElaveInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 9, 7, 6, 12, 15 };
+            int num = 2;
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = num;
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
